1bde9382b15271065725eb05624d41b7d2a5bc5d
Adrian Black <adrian.black@digimarc.com>
2018-04-15 21:22:19 -0700

    add missing ImageFile.cs

---
+++ lib/Whalerator/Model/ImageFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Whalerator.Model
+{
+    public class ImageFile
+    {
+        //public string Layer { get; set; }
+        public int LayerDepth { get; set; }
+        public string Path { get; set; }
+    }
+}
