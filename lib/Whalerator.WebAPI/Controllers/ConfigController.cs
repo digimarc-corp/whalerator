2574c2c330774894a71725d7c51f12919d074a40
Adrian Black <adrian.black@digimarc.com>
2018-05-18 11:42:02 -0700

    add config endpoint for spa

---
+++ lib/Whalerator.WebAPI/Controllers/ConfigController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Whalerator.WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/config")]
+    public class ConfigController : Controller
+    {
+        public ConfigController(ILogger<ConfigController> logger, Config config)
+        {
+            Logger = logger;
+            Config = config;
+        }
+
+        public ILogger<ConfigController> Logger { get; }
+        public Config Config { get; }
+
+        /// <summary>
+        /// Returns configuration options for the Whalerator UI SPA
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Registry = Config.Catalog?.Registry,
+                SearchPaths = Config.Search?.Filenames.ToArray() ?? new[] { "readme.md" }
+            });
+        }
+    }
+}
